3f1c1e899babc4d8cf9cd3029984e0d0826a73f7
Eric Howey <ehowey@talkroute.com>
2015-06-27 13:33:30 -0500

    Added New Files

---
+++ src/Model/Table/EmailsTable.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Model\Table;
+
+use Cake\Event\Event;
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+use Cake\ORM\TableRegistry;
+
+/**
+ * Hunts Model
+ */
+class EmailsTable extends Table {
+
+    public function initialize(array $config) {
+        $this->table(false);
+    }
+
+    private function getUser($id) {
+
+        if (!isset($id)) {
+            return "This method requires 1 argument";
+        }
+
+        $this->loadModel('Users');
+        $query = $this->Users
+                ->find()->where(['Users.id' => $id])
+                ->first();
+
+        return $query;
+    }
+
+    public function welcome($userId) {
+
+        if (!isset($userId)) {
+            return "This method requires 1 argument";
+        }
+
+        $user = $this->getUser($userId);
+
+        $email = new Email();
+        $email->from(['gm@vgphotohunt.com' => 'Game Master'])
+                ->to($user->email)
+                ->subject('Please Confirm Your Account')
+                ->template('welcome')
+                ->viewVars(['token' => $user->confirmation_token])
+                ->send();
+    }
+
+}
---
+++ src/Template/Email/text/welcome.ctp
@@ -0,0 +1,4 @@
+Hi. Please confirm your email by clicking the following link:
+<br />
+<br />
+https://vgphotohunt.com/users/verify/ <?= $token ?>
