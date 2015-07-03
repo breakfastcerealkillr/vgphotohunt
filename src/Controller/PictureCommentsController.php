69fdf73498f45809b488e68bdfdcad2420d72eb4
erichowey <ehowey@talkroute.com>
2015-07-02 20:46:13 -0500

    Git done messed up

--- src/Controller/PictureCommentsController.php
+++ src/Controller/PictureCommentsController.php
@@ -9,59 +9,53 @@ use App\Controller\AppController;
  *
  * @property \App\Model\Table\PictureCommentsTable $PictureComments
  */
-class PictureCommentsController extends AppController {
+class PictureCommentsController extends AppController
+{
 
     /**
      * Add method
      *
      * @return void Redirects on successful add, renders view otherwise.
      */
-    public function add() {
-<<<<<<< HEAD
-        if($this->user_id == $this->request->data['user_id']) {
-        $pictureComment = $this->PictureComments->newEntity();
-        if ($this->request->is('post')) {
-            $pictureComment = $this->PictureComments->patchEntity($pictureComment, $this->request->data);
-            if ($this->PictureComments->save($pictureComment)) {
-                //Get a query of the related picture, mark, and hunt
-                $this->loadModel('Pictures');
-                $query = $this->Pictures->get($pictureComment->picture_id, [
-                    'contain' => ['Marks.Hunts']
-                ]);
-                
-                //Check to make sure the user can't give themselves notifications
-                if ($this->user_id != $query->user_id) {
-                    $this->loadModel('Notifications');
-                    $this->Notifications->add($query->user_id,'huntcomment',$query->mark->hunt->id, $query->mark->name);
-                }
-                
-                $this->Flash->success('The picture comment has been saved.');
-            } else {
-                $this->Flash->error('The picture comment could not be saved. Please, try again.');
-            }
-=======
+    public function add()
+    {
 
         $this->loadModel('Users');
 
         if (!$this->Users->isVerified($this->user_id)) {
             $this->Flash->error('Please verify your email before commenting.');
             return $this->redirect($this->referer());
->>>>>>> master
         }
 
         if ($this->user_id == $this->request->data['user_id']) {
             $pictureComment = $this->PictureComments->newEntity();
             if ($this->request->is('post')) {
-                $pictureComment = $this->PictureComments->patchEntity($pictureComment, $this->request->data);
+                $pictureComment = $this->PictureComments->patchEntity($pictureComment,
+                    $this->request->data);
                 if ($this->PictureComments->save($pictureComment)) {
+                    //Get a query of the related picture, mark, and hunt
+                    $this->loadModel('Pictures');
+                    $query = $this->Pictures->get($pictureComment->picture_id,
+                        [
+                        'contain' => ['Marks.Hunts']
+                    ]);
+
+                    //Check to make sure the user can't give themselves notifications
+                    if ($this->user_id != $query->user_id) {
+                        $this->loadModel('Notifications');
+                        $this->Notifications->add($query->user_id,
+                            'huntcomment', $query->mark->hunt->id,
+                            $query->mark->name);
+                    }
+
                     $this->Flash->success('The picture comment has been saved.');
                 } else {
                     $this->Flash->error('The picture comment could not be saved. Please, try again.');
                 }
             }
-        }
 
-        return $this->redirect($this->referer());
+            return $this->redirect($this->referer());
+        }
     }
 
     /**
@@ -71,12 +65,15 @@ class PictureCommentsController extends AppController {
      * @return void Redirects on successful edit, renders view otherwise.
      * @throws \Cake\Network\Exception\NotFoundException When record not found.
      */
-    public function edit($id = null) {
-        $pictureComment = $this->PictureComments->get($id, [
+    public function edit($id = null)
+    {
+        $pictureComment = $this->PictureComments->get($id,
+            [
             'contain' => []
         ]);
         if ($this->request->is(['patch', 'post', 'put'])) {
-            $pictureComment = $this->PictureComments->patchEntity($pictureComment, $this->request->data);
+            $pictureComment = $this->PictureComments->patchEntity($pictureComment,
+                $this->request->data);
             if ($this->PictureComments->save($pictureComment)) {
                 $this->Flash->success('The picture comment has been saved.');
                 return $this->redirect(['action' => 'index']);
@@ -84,8 +81,9 @@ class PictureCommentsController extends AppController {
                 $this->Flash->error('The picture comment could not be saved. Please, try again.');
             }
         }
-        $pictures = $this->PictureComments->Pictures->find('list', ['limit' => 200]);
-        $users = $this->PictureComments->Users->find('list', ['limit' => 200]);
+        $pictures = $this->PictureComments->Pictures->find('list',
+            ['limit' => 200]);
+        $users    = $this->PictureComments->Users->find('list', ['limit' => 200]);
         $this->set(compact('pictureComment', 'pictures', 'users'));
         $this->set('_serialize', ['pictureComment']);
     }
@@ -97,7 +95,8 @@ class PictureCommentsController extends AppController {
      * @return void Redirects to index.
      * @throws \Cake\Network\Exception\NotFoundException When record not found.
      */
-    public function delete($id = null) {
+    public function delete($id = null)
+    {
         $this->request->allowMethod(['post', 'delete']);
         $pictureComment = $this->PictureComments->get($id);
         if ($this->PictureComments->delete($pictureComment)) {
@@ -108,7 +107,8 @@ class PictureCommentsController extends AppController {
         return $this->redirect(['action' => 'index']);
     }
 
-    public function adminAdd() {
+    public function adminAdd()
+    {
 
         $this->adminOnly();
 
@@ -128,13 +128,15 @@ class PictureCommentsController extends AppController {
         $this->set('users', $this->PictureComments->Users->find('list'));
     }
 
-    public function adminEdit($id = null) {
+    public function adminEdit($id = null)
+    {
 
         $this->adminOnly();
 
         $pictureComment = $this->PictureComments->get($id);
         if ($this->request->is(['patch', 'post', 'put'])) {
-            $pictureComment = $this->PictureComments->patchEntity($pictureComment, $this->request->data);
+            $pictureComment = $this->PictureComments->patchEntity($pictureComment,
+                $this->request->data);
             if ($this->PictureComments->save($pictureComment)) {
                 $this->Flash->success('Saved!');
                 return $this->redirect(['action' => 'adminEdit', $id]);
@@ -147,7 +149,8 @@ class PictureCommentsController extends AppController {
         $this->set('pictures', $this->PictureComments->Pictures->find('list'));
     }
 
-    public function adminDelete($id = null) {
+    public function adminDelete($id = null)
+    {
 
         $this->adminOnly();
 
@@ -160,5 +163,4 @@ class PictureCommentsController extends AppController {
 
         return $this->redirect($this->referer());
     }
-
 }
